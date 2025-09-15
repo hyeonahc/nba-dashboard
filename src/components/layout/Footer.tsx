baf9de99105bcb3534f50e2490200e5d9f29a68f
Hyeonah Cho <hyeonah.hello@gmail.com>
2025-09-15 18:33:34 -0400

    feat: comment out footer bottom border section

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -138,13 +138,13 @@ const Footer = () => {
         </div>
 
         {/* Bottom Border */}
-        <div className="border-t border-gray-800 mt-8 pt-8">
+        {/* <div className="border-t border-gray-800 mt-8 pt-8">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <p className="text-gray-400 text-sm mt-2 md:mt-0">
               Data provided by NBA API
             </p>
           </div>
-        </div>
+        </div> */}
       </div>
     </footer>
   )
